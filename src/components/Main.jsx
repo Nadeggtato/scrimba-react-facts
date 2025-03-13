abe63575ff6e0f8b7e6fe5adcdd829d66cbfddd3
ia <paepulumbarit@gmail.com>
2025-03-13 15:51:31 -0700

    Body + background

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,15 @@
 export default function Main()
 {
-    return (
-        <div>Main Component here</div>
-    )
+  return (
+    <main>
+      <h1>Fun Facts about React</h1>
+      <ul id="react-facts-list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Wilke</li>
+        <li>Has well over 200K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  )
 }
