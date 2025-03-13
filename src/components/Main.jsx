1b5fb0516eb05e03c83895841a8a642a45e2fee4
ia <paepulumbarit@gmail.com>
2025-03-13 13:50:56 -0700

    Initial commit + Navbar component

---
+++ src/components/Main.jsx
@@ -0,0 +1,6 @@
+export default function Main()
+{
+    return (
+        <div>Main Component here</div>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar()
+{
+  return (
+    <header>
+      <nav className="navbar">
+        <img src="/assets/images/react-logo.png"/>
+        <span>ReactFacts</span>
+      </nav>
+    </header>
+  )
+}
